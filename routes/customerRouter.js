50d546fc3f340636e975b0d25db2f456e4517d78
Duncan Gatuthu <dgatuthu@gmail.com>
2022-04-14 15:44:01 +0300

    added the customer routed handlers/endpoints

---
+++ routes/customerRouter.js
@@ -0,0 +1,41 @@
+import express from "express";
+import { PrismaClient } from "@prisma/client";
+
+const customerRouter = express.Router();
+const prisma = new PrismaClient();
+
+customerRouter.post("/customer", async (req, res) => {
+  const { customer_name, address, location } = req.body;
+
+  const customer = await prisma.customer.create({
+    data: {
+      customer_name,
+      address,
+      location,
+    },
+  });
+
+  res.json(customer);
+});
+
+customerRouter.get("/customers", async (res) => {
+  const allCustomers = await prisma.customer.findMany();
+
+  res.json(allCustomers);
+});
+
+customerRouter.put("/customer", async (req, res) => {
+  const { id, location } = req.body;
+  const updatedCustomer = await prisma.customer.update({
+    where: {
+      id: Number(id),
+    },
+    data: {
+      location,
+    },
+  });
+
+  res.json(updatedCustomer);
+});
+
+export default customerRouter;
