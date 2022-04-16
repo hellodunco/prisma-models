6e1709e4d4ff7dbcf0ea491ad8ad4759cd6ca1e5
Duncan G <dgatuthu@gmail.com>
2022-04-16 17:07:52 +0300

    Fixed a bug in the POST orders API handler

--- routes/orderRouter.ts
+++ routes/orderRouter.ts
@@ -1,26 +1,28 @@
-import express, { Response } from "express";
+import express, { Request, Response } from "express";
 import { PrismaClient } from "@prisma/client";
 
 const orderRouter = express.Router();
 const prisma = new PrismaClient();
 
-orderRouter.post("/addOrder", async (req, res) => {
-  const { order_no, customer_id, order_date, duration, status } = req.body;
+orderRouter.post("/order", async (req: Request, res: Response) => {
+  const { order_no, customer_id, order_date, duration, status, invoice_no } =
+    req.body;
 
   const Order = await prisma.order.create({
     data: {
       order_no,
       customer_id,
-      order_date,
-      duration,
+      order_date: new Date(),
+      duration: Number(duration),
       status,
+      invoice_no,
     },
   });
 
   res.json(Order);
 });
 
-orderRouter.get("/orders", async (res: Response) => {
+orderRouter.get("/orders", async (req: Request, res: Response) => {
   const allOrders = await prisma.order.findMany();
 
   res.json(allOrders);
