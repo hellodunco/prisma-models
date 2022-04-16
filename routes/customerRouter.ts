4b03add271fdf7bcdb6eecb41db8056612c11022
Duncan G <dgatuthu@gmail.com>
2022-04-16 17:06:53 +0300

    Renamed the routed and added GET req handlers

--- routes/customerRouter.ts
+++ routes/customerRouter.ts
@@ -1,4 +1,4 @@
-import express, { Response } from "express";
+import express, { Request, Response } from "express";
 import { PrismaClient } from "@prisma/client";
 
 const customerRouter = express.Router();
@@ -18,12 +18,26 @@ customerRouter.post("/customer", async (req, res) => {
   res.json(customer);
 });
 
-customerRouter.get("/customers", async (res: Response) => {
+// Get individual customer
+customerRouter.get("/customer", async (req: Request, res: Response) => {
+  const { id } = req.body;
+
+  const allCustomers = await prisma.customer.findUnique({
+    where: {
+      id: Number(id),
+    },
+  });
+
+  res.json(allCustomers);
+});
+
+customerRouter.get("/customers", async (req: Request, res: Response) => {
   const allCustomers = await prisma.customer.findMany();
 
   res.json(allCustomers);
 });
 
+// Update customer data
 customerRouter.put("/customer", async (req, res) => {
   const { id, location } = req.body;
   const updatedCustomer = await prisma.customer.update({
