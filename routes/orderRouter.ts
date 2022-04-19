b81499747c6913601a510d6b318eee55b3192ed5
Duncan G <dgatuthu@gmail.com>
2022-04-19 11:50:19 +0300

    Created the Orders route handlers

--- routes/orderRouter.ts
+++ routes/orderRouter.ts
@@ -1,31 +1,77 @@
 import express, { Request, Response } from "express";
 import { PrismaClient } from "@prisma/client";
 
-const orderRouter = express.Router();
+const orderRoute = express.Router();
 const prisma = new PrismaClient();
 
-orderRouter.post("/order", async (req: Request, res: Response) => {
-  const { order_no, customer_id, order_date, duration, status, invoice_no } =
+// Create order
+orderRoute.post("/order", async (req: Request, res: Response) => {
+  const { order_no, customer_id, items, order_date, duration, status } =
     req.body;
 
   const Order = await prisma.order.create({
     data: {
       order_no,
       customer_id,
-      order_date: new Date(),
+      items,
+      order_date,
       duration: Number(duration),
       status,
-      invoice_no,
     },
   });
 
   res.json(Order);
 });
 
-orderRouter.get("/orders", async (req: Request, res: Response) => {
+// Get all orders
+orderRoute.get("/orders", async (req: Request, res: Response) => {
   const allOrders = await prisma.order.findMany();
 
   res.json(allOrders);
 });
 
-export default orderRouter;
+// Get specific order
+orderRoute.get("/orders/:id", async (req: Request, res: Response) => {
+  const id = req.params.id;
+  const items = req.body.items;
+
+  const order = await prisma.order.findUnique({
+    where: {
+      order_no: id,
+    },
+  });
+
+  res.json(order);
+});
+
+// Update order
+orderRoute.get("/orders/:id", async (req: Request, res: Response) => {
+  const id = req.params.id;
+  const items = req.body.items;
+
+  const updatedOrder = await prisma.order.update({
+    where: {
+      order_no: id,
+    },
+    data: {
+      items: JSON.parse(items),
+    },
+  });
+
+  res.json(updatedOrder);
+});
+
+// Delete order
+orderRoute.get("/orders/:id", async (req: Request, res: Response) => {
+  const order_no = req.params.id;
+
+  const deletedOrder = await prisma.order.delete({
+    where: {
+      order_no,
+    },
+  });
+
+  res.json(deletedOrder);
+});
+
+export default orderRoute;
